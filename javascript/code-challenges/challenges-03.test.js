deafa4c00397ca9b569acffd1336ab92602c4b4b
Marcus Hartwig <marcusdnhartwig@gmail.com>
2022-03-16 16:53:54 -0700

    comp 5of9

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -68,8 +68,8 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 
 const notInFirstArray = (forbiddenValues, arr) => {
   // Solution code here...
-  const giveFive = arr.filter.includes(item = typeof item === []);
-  return giveFive;
+  const giveArr = arr.filter(item => !forbiddenValues.includes(item));
+  return giveArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
